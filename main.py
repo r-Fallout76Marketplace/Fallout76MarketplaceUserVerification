29776c527332a99ba1ef321a16c3e87547f771d6
isFakeAccount <trevorphillips@gmx.us>
2022-10-02 03:11:51 -0400

    Updated the website UI and added button for Login
    Signed-off-by: isFakeAccount <trevorphillips@gmx.us>

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from os import getenv
+
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 
+@app.route('/login/callback', methods=["GET"])
+def reddit_oauth_callback():
+    if error := request.args.get('error'):
+        if error == "access_denied":
+            return redirect("https://http.cat/403")
+        else:
+            return error
+    else:
+        return redirect("https://www.google.com/")
+
+
+@app.route('/reddit_oauth', methods=["GET"])
+def reddit_oauth():
+    return redirect("https://www.reddit.com/api/v1/authorize?"
+                    f"client_id={getenv('CLIENT_ID')}"
+                    "response_type=code&"
+                    "state=fallout&"
+                    "redirect_uri=http://localhost:5000/login/callback&"
+                    "duration=permanent&"
+                    "scope=identity", code=302)
+
+
 @app.route('/')
-def index():  # put application's code here
+def index():
     return render_template('index.html')
 
 
