d2c0c5ad21769a2ef64f16703c8eb960fc7663b6
isFakeAccount <trevorphillips@gmx.us>
2022-10-29 18:01:03 -0400

    slight formatting in logs
    Signed-off-by: isFakeAccount <trevorphillips@gmx.us>

--- profile.py
+++ profile.py
@@ -35,7 +35,7 @@ def user_profile(user_name: str):
     user_name = user_name.lower()
     fetch_res = deta_api.get_item(user_name)
 
-    logger.info(f"{user_name} profile visited by {session.get('username', '!NotLoggedIn')}"
+    logger.info(f"{user_name} profile visited by {session.get('username', '!NotLoggedIn')} "
                 f"{request.headers.get('Cf-Connecting-Ip', request.remote_addr)} {request.headers.get('User-Agent')}.")
     # If user doesn't exist in db
     if fetch_res.count == 0 or not fetch_res.items[0].get("verification_complete"):
